34ff1e4e0bad7fc8d89d2532b3d152520925e458
aa <acardenas@sweetcherrytech.com>
2019-06-19 22:39:11 -0500

    Create migration for shortened urls

--- database/migrations/2019_06_20_011516_create_shortened_urls_table.php
+++ database/migrations/2019_06_20_011516_create_shortened_urls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateShortenedUrlsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shortened_urls', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('url');
+            $table->string('code');
+            $table->string('shortened_url');
+            $table->string('title')->nullable();
+            $table->bigInteger('times_visited')->default(0);
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shortened_urls');
     }
 }
